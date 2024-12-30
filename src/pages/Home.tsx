a382b395db8514e962164654119115917f185d48
Wesley Fu <wesleyfu@Wesleys-Air.lan>
2024-12-30 17:37:29 +1100

    cleaned up no_colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -19,28 +19,28 @@ export default function Home(){
                             <span className="ml-[10px]">Get Started</span>
                         </div>
                     </div>
-                    <div className="w-full lg:col-span-5 relative">
+                    <div className="w-full lg:col-span-5 relative hidden lg:block">
                         <div className="bg-[url('src/assets/hero-img.png')] top-[20px] right-[-40px] relative min-h-[460px] min-w-[590px] bg-no-repeat rounded-lg"></div>
                     </div>
                 </div>
             </Section>
-            <Section className="min-h-[500px] mt-[20px]">
+            <Section className="pt-[60px] pb-[50px] bg-babyblue text-navy min-h-[500px] mt-[20px]">
                 <div className="border border-pink text">
                     <h2>Lorem ipsum odor amet.</h2>
                     <p className="max-w-[740px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis, scelerisque interdum sapien molestie facilisis pharetra.</p>
                 </div> 
-                <div className="mt-[10px] text flex-col flex md:flex-row justify-center gap-8 border border-orange-400">
-                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                <div className="mt-[10px] text flex-col flex md:flex-row justify-center gap-8">
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white cards">
                             <h3>Connect</h3>
-                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis.</p>
+                            <p className="mt-[20px] text-[1.6rem]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis nullam eu viverra porta fusce. viverra porta fusce</p>
                         </div>
-                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white cards">
                             <h3>Book</h3>
-                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis.</p>
+                            <p className="mt-[20px] text-[1.6rem]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis nullam eu viverra porta fusce. nullam eu viverra</p>
                         </div>
-                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white cards">
                             <h3>Manage</h3>
-                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla.</p>
+                            <p className="mt-[20px] text-[1.6rem]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla nullam eu viverra porta fusce. nullam eu viverra porta </p>
                         </div>
                 </div>
             </Section>
