756e0d284043efcab3e7448804e6f0964b5ff8cd
Wesley Fu <wesleyfu@Wesleys-Air.lan>
2024-12-30 16:56:01 +1100

    ADDED: section-3-cards strcuture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,14 +9,14 @@ export default function Home(){
             <Section className="min-h-[500px]">
                 <div className="grid h-full lg:grid-cols-12">
                     <div className="w-full mt-[70px] lg:col-span-7 bg-red-500">
-                        <div className="border border-white p-[20px]">
+                        <div className="border border-white text">
                             <p>Welcome to EclipseCare</p>
                             <h1>Your Life Our Mission.</h1>
                             <p className="border border-white max-w-[530px]">We're committed to our family and providing the best outcomes possible for participants and their goals in society.</p>
-                        </div>
-                        <div className="p-[20px]">
-                            <button>Learn More</button>
-                            <span className="ml-[10px]">Get Started</span>
+                            <div className="mt-[20px]">
+                                <button>Learn More</button>
+                                <span className="ml-[10px]">Get Started</span>
+                            </div>
                         </div>
                     </div>
                     <div className="w-full lg:col-span-5 bg-blue-500">
@@ -24,11 +24,24 @@ export default function Home(){
                     </div>
                 </div>
             </Section>
-            <Section className="h-[500px]">
-                <div className="md:flex justify-center gap-8 border border-orange-400">
-                        <div className="w-full md:max-w-[300px] rounded-lg h-[250px] border border-white"></div>
-                        <div className="w-full md:max-w-[300px] rounded-lg h-[250px] border border-white"></div>
-                        <div className="w-full md:max-w-[300px] rounded-lg h-[250px] border border-white"></div>
+            <Section className="min-h-[500px] mt-[20px]">
+                <div className="border border-pink text">
+                    <h2>Lorem ipsum odor amet.</h2>
+                    <p className="max-w-[740px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis, scelerisque interdum sapien molestie facilisis pharetra.</p>
+                </div> 
+                <div className="mt-[10px] text flex-col flex md:flex-row justify-center gap-8 border border-orange-400">
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                            <h3>Connect</h3>
+                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis.</p>
+                        </div>
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                            <h3>Book</h3>
+                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla quis.</p>
+                        </div>
+                        <div className="text w-full md:max-w-[300px] rounded-lg min-h-[200px] border border-white">
+                            <h3>Manage</h3>
+                            <p className="mt-[20px]">Tempus nisl vivamus efficitur etiam libero. Ac odio class nullam eu viverra porta fusce diam! Velit fringilla.</p>
+                        </div>
                 </div>
             </Section>
         </>
